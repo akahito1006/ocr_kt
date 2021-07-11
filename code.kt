5473b39ca680de67a951f21d019353116e6b5b90
akahito1006 <nekodora5k@gmail.com>
2021-07-11 21:23:45 +0900

    hello kotlin

--- code.kt
+++ code.kt
@@ -1,1 +1,15 @@
-code.kt
+val zero = 0
+val message = "Hello world !"
+fun sayHello() = "hello everyone"
+
+private lateinit var answer: String
+private val question = "What is your name?"
+fun main(args: Array<String>) {
+    println(question)
+    answer = "akahito"
+    println("Your name is $answer")
+        
+}
+// $
+// What is your name?
+// Your name is akahito
