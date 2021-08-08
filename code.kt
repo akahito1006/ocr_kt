d23a925d5096a06660b3ba3537a06273053101e4
akahito1006 <nekodora5k@gmail.com>
2021-08-08 19:01:12 +0900

    SQL on Kotlin improved its readability

--- code.kt
+++ code.kt
@@ -23,3 +23,11 @@ var kotlinVariable: String = when (var fortuneParams) {
 }
 println(kotlinVariable)
 // 大吉
+
+// SQL文と可読性
+var sql: String = "INSERT INTO customer_list (first_name, last_name, age) VALUES ('Wilman', 'Kala', '23')"
+
+var organizedSQL: String = "INSERT INTO "
+organizedSQL += "product_list (item, name, price) "
+organizedSQL += "VALUES ('vaporizer', 'eGo ONE', '4,000')"
+// INSERT INTO product_list (item, name, price) VALUES ('vaporizer', 'eGo ONE', '4,000')
